d1f3c6e58d50142583f6c9adbde5365dde8e010f
geo-json <59700334+geo-json@users.noreply.github.com>
2020-04-12 10:47:23 -0400

    Created jupyter notebooks file
    Plotting was not working on VS code

--- greatlakesIce_project.py
+++ greatlakesIce_project.py
@@ -5,21 +5,24 @@ from datetime import datetime
 
 df = pd.read_table('/Users/vineethjason/Documents/University_of_Michigan/Winter_2020/EAS_639_006/PythonB/pythonB_assignments/weatherData/PTIM4/ptim4h2005.txt', delim_whitespace=True, names=("YYYY","MM","DD","hh","mm","WD","WSPD","GST","WVHT","DPD","APD","MWD","BAR","ATMP","WTMP","DEWP","VIS","TIDE"), low_memory=False)
 df["dateTime"] = df["YYYY"].astype(str) + df["MM"].astype(str) + df["DD"].astype(str) + df["hh"].astype(str) + df["mm"].astype(str)
+df = df.loc[1:, :]
 
-dfList = df['dateTime'][1:].tolist()
+dfList = df['dateTime'].tolist()
 dateList = []
 for d in dfList:
     dates = datetime.strptime(d, '%Y%m%d%H%M')
     dateList.append(dates)
 
-a = ["YYYYMMDDhhmm"]
-dateListnew = a + dateList
-df["dateTimeTrue"] = dateListnew
+#a = ["YYYYMMDDhhmm"]
+#dateListnew = a + dateList
+df["dateTimeTrue"] = dateList
 
-df['ATMP'][1:] = df['ATMP'][1:].astype(float)
+df['ATMP'] = df['ATMP'].astype(float)
 
-print(type(df['ATMP'][1]))
+#print(type(df['ATMP'][1]))
 
-dfNew = df.loc[1:, ["dateTimeTrue", "ATMP"]]
+dfNew = df.loc[:, ["dateTimeTrue", "ATMP"]]
 #print(dfNew.head())
-df[["ATMP"][1:]].plot()
+#print(type(df[["ATMP"][1:]]))
+
+dfNew.plot(kind='line',x='ATMP',y='dateTimeTrue')
